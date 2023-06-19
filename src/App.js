b79bc99f08efe134acca6988c4a2c2b1ada7c28a
Sambuka <vova.kurtiak@gmail.com>
2023-06-19 18:21:38 +0200

    app done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react'
+import axios from 'axios'
+
+import styles from './app.module.scss'
 
 function App() {
+  const [advice, setAdvice] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const adviceRef = useRef(advice)
+  adviceRef.current = advice
+  const errorRef = useRef(error)
+  errorRef.current = error
+
+  const initialized = useRef(false)
+  const TIMEOUT_INTERVAL = 500
+
+  const loadAdvice = async () => {
+    setLoading(true)
+    
+    if(advice) setAdvice(null)
+
+    //show an update after timeout
+    const timeoutId = setTimeout(() => {
+      if (!adviceRef.current && !errorRef.current) {
+        setError(new Error("This is taking longer than usual..."))
+      }
+    }, TIMEOUT_INTERVAL)
+
+    //request
+    try {
+      const response = await axios.get(`https://api.adviceslip.com/advice`)
+      setAdvice(response.data?.slip);
+      setLoading(false)
+      setError(null)
+    } catch (err) {
+      setError(err)
+      setLoading(false)
+    } finally {
+      clearTimeout(timeoutId)
+    }
+  }
+
+  useEffect(() => {
+    if(!initialized.current) {
+      initialized.current = true
+
+      loadAdvice() 
+    }
+  }, [])
+
+  console.log(advice);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        {error ? 
+          <div className={styles.error}>{error.message}</div> :
+          !advice || loading ? <div className={styles.loading}>loading...</div> :
+          <>
+            <div className={styles.header}>
+              advice #{advice.id}
+            </div>
+            <div className={styles.advice}>
+              <span>"{advice.advice}"</span>
+            </div>
+              <div className={styles.line}>
+                <img src='/images/pattern-divider-desktop.svg' alt='divider icon'/>  
+            </div>
+          </>
+        }
+      </div>
+      <div className={styles.button}>
+        <img src='/images/icon-dice.svg' alt='dice icon' onClick={loadAdvice}/>
+      </div>
     </div>
   );
 }
